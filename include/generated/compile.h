848a57773fe941bb85a4d31f5dae2824ad4c805f
CheUhxg <3193079152@qq.com>
2024-10-17 16:27:19 +0800

    init: v6.6

---
+++ include/generated/compile.h
@@ -0,0 +1,4 @@
+#define UTS_MACHINE		"x86_64"
+#define LINUX_COMPILE_BY	"walle"
+#define LINUX_COMPILE_HOST	"walle-ZHENGJIUZHE-REN7000K-28IMB"
+#define LINUX_COMPILER		"gcc (Ubuntu 9.4.0-1ubuntu1~20.04.2) 9.4.0, GNU ld (GNU Binutils for Ubuntu) 2.34"
